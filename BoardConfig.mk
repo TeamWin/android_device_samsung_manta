1c5acf1446b5d134b6d5a94b8b3a6eac32553eee
Menno van Grinsven <scanno71@gmail.com>
2014-11-30 21:05:25 +0100

    manta: symlinks added so battery percentage and charge status are shown in TWRP
    Change-Id: I39a23d0d51bd169356cb6f4770475113e45e06e9

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -81,12 +81,12 @@ BOARD_HAL_STATIC_LIBRARIES := libhealthd.manta libdumpstate.manta
 
 BOARD_CHARGER_DISABLE_INIT_BLANK := true
 
-TARGET_RECOVERY_UPDATER_LIBS += librecovery_updater_manta
+#TARGET_RECOVERY_UPDATER_LIBS += librecovery_updater_manta
 TARGET_RELEASETOOLS_EXTENSIONS := device/samsung/manta
 
 #TWRP
 TARGET_PREBUILT_KERNEL := device/samsung/manta/kernel
-TARGET_RECOVERY_UI_LIB := librecovery_ui_manta
+#TARGET_RECOVERY_UI_LIB := librecovery_ui_manta
 TARGET_RECOVERY_UPDATER_LIBS += librecovery_updater_manta
 TARGET_RECOVERY_PIXEL_FORMAT := RGBX_8888
 DEVICE_RESOLUTION := 2560x1600
